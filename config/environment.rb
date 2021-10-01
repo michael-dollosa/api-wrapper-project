716dc5c5fafe62bdcd7b5f1d5ef53a5ed0c55362
Michael Dollosa <michaeldollosa@dolees-Macbook-Pro.local>
2021-10-01 21:40:03 +0800

    added config host in environment rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the Rails application.
 require_relative "application"
-
+config.hosts << "challonge-api-wrapper.herokuapp.com"
 # Initialize the Rails application.
 Rails.application.initialize!
